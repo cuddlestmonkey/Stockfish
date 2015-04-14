2baa084fcf0219bcd94c9f4f74edcf0c018bdd1d
Vince Negri <vince.negri@gmail.com>
2015-04-14 07:03:59 +0100

    Prep for tuning

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -77,7 +77,8 @@ namespace {
   { V( 80), V(12), V(43), V(65), V(88), V( 91), V(115) } };
 
   // Value of a doubled pawn in front of the king by [distance from edge]
-  const Value ExtraPawnStrength[] = { V(0), V(7), V(12), V(10) };
+  Value ExtraPawnStrength[] = { V(0), V(7), V(12), V(10) };
+TUNE(SetRange(0,30), ExtraPawnStrength);
 
   // Danger of enemy pawns moving toward our king by [type][distance from edge][rank]
   const Value StormDanger[][4][RANK_NB] = {
