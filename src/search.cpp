21f5faa778f7ced9aec41decd9f2942c1112a304
Vince Negri <vince.negri@gmail.com>
2015-05-16 10:17:14 +0100

    Lower verification search thresh for null move

--- src/search.cpp
+++ src/search.cpp
@@ -734,7 +734,7 @@ namespace {
             if (nullValue >= VALUE_MATE_IN_MAX_PLY)
                 nullValue = beta;
 
-            if (depth < 12 * ONE_PLY && abs(beta) < VALUE_KNOWN_WIN)
+            if (depth < 5 * ONE_PLY && abs(beta) < VALUE_KNOWN_WIN)
                 return nullValue;
 
             // Do verification search at high depths
