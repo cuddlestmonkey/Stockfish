b1da0ffbe254694b40ee9dbd4bc4501a57b6bd65
Vince Negri <vince.negri@gmail.com>
2015-05-28 18:11:02 +0100

    Change threshold to .85

--- src/search.cpp
+++ src/search.cpp
@@ -464,7 +464,7 @@ namespace {
             double a, b;
             Statistics::linear_fit(xiteration, lognodes, depth, a, b);
 
-            if (b > 0.78) {
+            if (b > 0.85) {
                 //std::cerr << "B = " << b << "so widen" << std::endl;
                 focus_search = true;
             }
